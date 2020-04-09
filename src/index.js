7f90e54fe9ac0257c0bcf10326baf3094e84d6c0
KulkarniBaswaraj <kulkarnibaswaraj@gmail.com>
2020-04-09 16:40:39 +0530

    replaced cors() line

--- src/index.js
+++ src/index.js
@@ -2,19 +2,18 @@ const express = require('express');
 const userRouter = require('./routers/user');
 const taskRouter = require('./routers/task');
 
-const cors = require('cors')
+const cors = require('cors');
 require("./db/mongoose");
-
 const app = express();
 const port = process.env.PORT;
 
 app.use(express.json());
+app.use(cors());
 
 //API Routes
 app.use(userRouter);
 app.use(taskRouter);
 
-app.use(cors())
 
 app.listen(port, () => {
    console.info("Node server up on port ", port);
